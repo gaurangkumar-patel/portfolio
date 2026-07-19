79b9c19838a53183a7e9ca2b1d1a144a806ad9aa
Gaurang Patel <gaurang.technogic@gmail.com>
2026-07-19 23:54:46 +0100

    feat: add SEO metadata sitemap and structured data

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   site: "https://gaurangkumar-patel.github.io",
   base: "/portfolio",
-  integrations: [react()],
+  trailingSlash: "always",
+  integrations: [react(), sitemap()],
 });
