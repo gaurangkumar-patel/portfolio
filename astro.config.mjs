65845eb3cc3fa436f33a93fca9025beadd0d20b3
Gaurang Patel <gaurang.technogic@gmail.com>
2026-07-19 19:04:42 +0100

    feat: create skeuomorphic portfolio foundation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
-import react from '@astrojs/react';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: "https://gaurangkumar-patel.github.io",
+  base: "/portfolio",
+  integrations: [react()],
 });
